33f48f6d8f0a88c5429851d75569992c3cc93f00
Rob Burley <rcburley@icloud.com>
2019-04-28 00:23:11 +0100

    Write tests for home page

---
+++ tests/Feature/HomePageTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class HomePageTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function see_articles()
+    {
+        $articles = factory('App\Article', 4)
+            ->create()
+            ->sortByDesc('published_at');
+
+        factory('App\Article')->create(['views' => 999]);
+
+        $this->get('/')
+            ->assertSeeInOrder($articles->pluck('title')->toArray());
+    }
+
+    /** @test */
+    public function see_most_popular_article()
+    {
+        $articles = factory('App\Article', 4)->create()->sortByDesc('published_at');
+        $article = factory('App\Article')->create(['views' => 999]);
+
+        $this->get('/')
+            ->assertSee($article->title)
+            ->assertSeeInOrder($articles->pluck('title')->toArray());
+    }
+
+    /** @test */
+    public function see_categories()
+    {
+        $categories = factory('App\Category', 5)->create()
+            ->sortBy('name')
+            ->map(function ($category) {
+                return ucwords($category->name);
+            });
+
+        $this->get('/')
+            ->assertSeeInOrder($categories->toArray());
+    }
+}
