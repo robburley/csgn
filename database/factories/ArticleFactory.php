fd49f489e1b5d64a0aa83590ee1e9fe7e25ebbaf
Rob Burley <rcburley@icloud.com>
2019-04-28 00:25:15 +0100

    Seed data

---
+++ database/factories/ArticleFactory.php
@@ -0,0 +1,14 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\Article;
+use Faker\Generator as Faker;
+
+$factory->define(Article::class, function (Faker $faker) {
+    return [
+        'title' => $faker->sentence(4),
+        'views' => $faker->numberBetween(0, 1000),
+        'published_at' => $faker->dateTimeThisMonth,
+    ];
+});
