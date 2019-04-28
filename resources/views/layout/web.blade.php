5a0f8f859309a103c3e6dedf8a2459d0b9cd38c7
Rob Burley <rcburley@icloud.com>
2019-04-28 18:39:35 +0100

    remove unused style sheet

--- resources/views/layout/web.blade.php
+++ resources/views/layout/web.blade.php
@@ -16,7 +16,6 @@
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
           integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/brands.css"
 </head>
 
 <body class="bg-orange-lightest text-black leading-normal">
