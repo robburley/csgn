6ea891535973e1f616b35df51c5ac948c6fa0221
Rob Burley <rcburley@icloud.com>
2019-04-28 10:32:27 +0100

    Add category to article display and update styling

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,11 +6,11 @@
     <div class="flex flex-wrap -mx-4">
         @if($featured)
             <div class="flex w-full px-4 mb-8">
-                <div class="w-full bg-grey-lightest shadow-md">
+                <div class="w-full bg-black shadow-md">
                     <img src="https://via.placeholder.com/1200x600" class="w-full border-b-4 border-orange">
 
                     <div class="px-2 py-4">
-                        <h3 class="font-normal text-grey-dark">
+                        <h3 class="font-normal text-grey-lightest">
                             {{ $featured->title }}
                         </h3>
                     </div>
@@ -26,6 +26,8 @@
                 >
 
                 <div class="py-2">
+                    <p class="pb-2 font-normal text-grey text-sm">{{ $article->category->name }}</p>
+
                     <h3 class="font-normal text-grey-dark">
                         {{ $article->title }}
                     </h3>
