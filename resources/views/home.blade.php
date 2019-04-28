8df2879b082476d590b7d59a09d2a19d02ec262d
Rob Burley <rcburley@icloud.com>
2019-04-28 12:35:58 +0100

    Update responsive layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -42,15 +42,15 @@
         </div>
 
         @foreach($articles as $article)
-            <div class="w-full sm:w-1/2 lg:w-1/3 px-4 mb-4">
-                <a class="w-full" href="{{ url('/') }}">
+            <div class="w-full lg:w-1/3 px-4 mb-4 flex flex-row lg:flex-col">
+                <a class="w-1/2 lg:w-full mr-4 lg:mr-0" href="{{ url('/') }}">
                     <img src="https://via.placeholder.com/400x200"
                          alt="{{ $article->title }}"
                          class="w-full border-b-4 border-orange"
                     >
                 </a>
 
-                <div class="py-2 flex flex-col">
+                <div class="flex flex-col w-1/2 lg:w-full py-2">
                     <div class="flex justify-between items-end pb-2 text-sm">
                         <a class="font-normal text-grey no-underline hover:text-orange"
                            href="{{ url('/') }}"
