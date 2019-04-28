5f5b8e1944068f7f348843356ee7cc9625ddcbdb
Rob Burley <rcburley@icloud.com>
2019-04-28 12:27:59 +0100

    Add comments count to each article

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,16 +5,27 @@
 @section('content')
     <div class="flex flex-wrap -mx-4">
         @if($featured)
-            <div class="flex w-full px-4 mb-4">
+            <div class="flex w-full px-4 mb-6">
                 <div class="w-full bg-black shadow-md">
-                    <img src="https://via.placeholder.com/1200x600" class="w-full border-b-4 border-orange">
+                    <a class="w-full" href="{{ url('/') }}">
+                        <img src="https://via.placeholder.com/1200x600" class="w-full border-b-4 border-orange">
+                    </a>
 
                     <div class="px-2 py-4 flex flex-col">
-                        <a class="pb-2 font-normal text-grey text-sm no-underline hover:text-orange"
-                           href="{{ url('/') }}"
-                        >
-                            {{ $featured->category->name }}
-                        </a>
+                        <div class="flex justify-between items-end pb-2 ">
+                            <a class="font-normal text-grey text-sm no-underline hover:text-orange"
+                               href="{{ url('/') }}"
+                            >
+                                {{ $featured->category->name }}
+                            </a>
+
+                            <a class="text-grey-lightest text-sm flex items-center no-underline hover:text-orange"
+                               href="{{ url('/') }}"
+                            >
+                                {{ $featured->comments_count }}
+                                <i class="fa fa-comment text-orange pl-1"></i>
+                            </a>
+                        </div>
 
                         <a class="font-normal text-grey-lightest no-underline hover:text-orange text-xl"
                            href="{{ url('/') }}"
@@ -26,23 +37,31 @@
             </div>
         @endif
 
-        <div class="w-full px-4 mb-4">
+        <div class="w-full px-4 mb-6">
             <h2>Latest Articles</h2>
         </div>
 
         @foreach($articles as $article)
             <div class="w-full sm:w-1/2 lg:w-1/3 px-4 mb-4">
-                <img src="https://via.placeholder.com/400x200"
-                     alt="{{ $article->title }}"
-                     class="w-full border-b-4 border-orange"
-                >
+                <a class="w-full" href="{{ url('/') }}">
+                    <img src="https://via.placeholder.com/400x200"
+                         alt="{{ $article->title }}"
+                         class="w-full border-b-4 border-orange"
+                    >
+                </a>
 
                 <div class="py-2 flex flex-col">
-                    <a class="pb-2 font-normal text-grey text-sm no-underline hover:text-orange"
-                       href="{{ url('/') }}"
-                    >
-                        {{ $article->category->name }}
-                    </a>
+                    <div class="flex justify-between items-end pb-2 text-sm">
+                        <a class="font-normal text-grey no-underline hover:text-orange"
+                           href="{{ url('/') }}"
+                        >
+                            {{ $article->category->name }}
+                        </a>
+
+                        <div class="text-grey flex items-center">
+                            {{ $featured->comments_count }} <i class="fa fa-comment text-orange pl-1"></i>
+                        </div>
+                    </div>
 
                     <a class="font-normal text-black no-underline hover:text-orange text-xl"
                        href="{{ url('/') }}"
